9444d7097d3c52920511fb2b10c3577898effe8e
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-11 13:02:33 -0300

    App.css substituido por styled-components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import './App.css';
 import Header from './components/Header';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(90deg, #002f52 35%, #216589 165%);
+`
 
 function App() {
 
   return (
-    <div className='App'>
+    <AppContainer>
       <Header />
-    </div>
+    </AppContainer>
   );
 }
 
