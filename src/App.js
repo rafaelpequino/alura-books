aca649f5986bb5030c9ad97cffce5fc456130002
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-11 12:29:47 -0300

    Adiciona opções do header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import './App.css';
 import Logo from './components/Logo';
 
 function App() {
+
+  const textoOpcoes = [
+    'CATEGORIAS',
+    'FAVORITOS',
+    'MINHA ESTANTE'
+  ]
+
   return (
     <div className='App'>
       <header className='App-header'>
         <Logo />
+        <ul className='opcoes'>
+          { textoOpcoes.map(texto => <li className='opcao'>{texto}</li>) }
+        </ul>
       </header>
     </div>
   );
