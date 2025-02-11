9f2b897fb8e161f0942e5c627ca9c4409936d525
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-11 14:19:34 -0300

    Altera estilos do InputPesquisa

--- src/components/InputPesquisa/Index.js
+++ src/components/InputPesquisa/Index.js
@@ -4,15 +4,16 @@ const InputPesquisa = styled.input`
     border: 1px solid #FFF;
     background: transparent;
     border: 1px solid #FFF;
-    padding: 20px 140px;
+    padding: 20px 40px;
     border-radius: 50px;
-    width: 200px;
+    width: 400px;
     color: #FFF;
     font-size: 16px;
     margin-bottom: 10px;
+    text-align: center;
 
     &::placeholder {
-        color: #FFF;
+        color:rgba(255, 255, 255, 0.34);
         font-size: 16px;
     }
 `
