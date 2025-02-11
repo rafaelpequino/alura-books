fab80094cd078653addb7c6eb0878c0a93c64b85
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-11 11:19:14 -0300

    Cria componente Logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
+import Logo from './components/Logo';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <div className='logo'>
-          <img src='/img/logo.svg' alt='Logo da Alura Books' />
-          <p><strong>Alura</strong>Books</p>
-        </div>
+        <Logo />
       </header>
     </div>
   );
---
+++ src/components/Logo/index.js
@@ -0,0 +1,12 @@
+import './Logo.css';
+
+const Logo = () => {
+    return (
+        <div className='logo'>
+            <img src='/img/logo.svg' alt='Logo da Alura Books' />
+            <p><strong>Alura</strong>Books</p>
+        </div>
+    )
+}
+
+export default Logo
