cb857613f16a782919e6e187a00b36de305c43b8
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-11 15:36:09 -0300

    Cria lista de livros

---
+++ src/components/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,27 @@
+export const livros = [
+    {
+        nome: 'Hamlet',
+        id: 1,
+        src: '/img/livro.png' 
+    },
+    {
+        nome: 'Como enfrentar o ódio',
+        id: 2,
+        src: '/img/livro.png' 
+    },
+    {
+        nome: 'Ainda estou aqui',
+        id: 3,
+        src: '/img/livro.png' 
+    },
+    {
+        nome: 'Solitária',
+        id: 4,
+        src: '/img/livro.png' 
+    },
+    {
+        nome: 'Corpos secos',
+        id: 5,
+        src: '/img/livro.png' 
+    },
+]
