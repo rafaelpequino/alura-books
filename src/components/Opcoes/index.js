a394ff909b75cd7d4bb2c9a90f513d5e1ac8819c
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-11 12:40:20 -0300

    Cria componente Opcoes

---
+++ src/components/Opcoes/index.js
@@ -0,0 +1,18 @@
+import './Opcoes.css'
+
+const Opcoes = () => {
+
+    const textoOpcoes = [
+        'CATEGORIAS',
+        'FAVORITOS',
+        'MINHA ESTANTE'
+    ]
+
+    return (
+        <ul className='opcoes'>
+            {textoOpcoes.map(texto => <li className='opcao'>{texto}</li>)}
+        </ul>
+    )
+}
+
+export default Opcoes;
