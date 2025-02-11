b76928d855cd92acf7185175b34008aecc0e68d5
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-11 11:14:45 -0300

    Cria header direto no App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className=''>
-
+    <div className='App'>
+      <header className='App-header'>
+        <div className='logo'>
+          <img src='/img/logo.svg' alt='Logo da Alura Books' />
+          <p><strong>Alura</strong>Books</p>
+        </div>
       </header>
     </div>
   );
