2a4d15f10bf21d4cdd4d4b4c6a4c000cce659063
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-11 12:34:29 -0300

    Adiciona ícones no header

--- src/App.js
+++ src/App.js
@@ -9,12 +9,28 @@ function App() {
     'MINHA ESTANTE'
   ]
 
+  const icones = [
+    {
+      imagem: '/img/sacola.svg',
+      alt: 'Ícone de sacola'
+    },
+    {
+      imagem: '/img/perfil.svg',
+      alt: 'Ícone de perfil'
+    }
+  ]
+
   return (
     <div className='App'>
       <header className='App-header'>
         <Logo />
+
         <ul className='opcoes'>
           { textoOpcoes.map(texto => <li className='opcao'>{texto}</li>) }
+        </ul>
+
+        <ul className='opcoes'>
+          { icones.map(icone => <li className='opcaoIcone'><img src={icone.imagem} alt='icone.alt' /></li>) }
         </ul>
       </header>
     </div>
