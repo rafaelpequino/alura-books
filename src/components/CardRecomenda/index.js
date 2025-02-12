643a9fbbe5aef87600f8a6823dab0c5aa6e4b98c
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-12 10:09:11 -0300

    Cria componente CardRecomenda

---
+++ src/components/CardRecomenda/index.js
@@ -0,0 +1,13 @@
+const CardRecomenda = (props) => {
+    return (
+        <div>
+            <h3>Talvez você se interesse por...</h3>
+            <h4>Angular 11</h4>
+            <p>Construindo uma aplicação integrada</p>
+            <img />
+            <button>Saiba mais</button>
+        </div>
+    )
+}
+
+export default CardRecomenda;
