e5f542ac0386aab23de61f2509f2c1da110341f9
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-02-12 12:23:07 -0300

    Cria o componente CardRecomenda

--- src/components/CardRecomenda/index.js
+++ src/components/CardRecomenda/index.js
@@ -1,13 +1,63 @@
-const CardRecomenda = (props) => {
+import styled from "styled-components"
+import { Titulo } from "../Titulo"
+
+const Card = styled.div`
+    align-items: center;
+    background-color: #FFF;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    display: flex;
+    margin: 0 auto;
+    max-width: 600px;
+    padding: 25px 20px;
+    justify-content: space-around;
+    width: 100%;  
+`
+
+const Botao = styled.button`
+    background-color: #EB9B00;
+    color: #FFF;
+    padding: 10px 0px;
+    font-size: 16px;
+    border: none;
+    font-weight: 900;
+    display: block;
+    text-align: center;
+    width: 150px;
+    &:hover {
+        cursor: pointer;
+    }
+`
+
+const Descricao = styled.p`
+    max-width: 300px;
+`
+
+const Subtitulo = styled.h4`
+    color: #002F52;
+    font-size: 18px;
+    font-weight: bold;
+    margin: 15px 0;
+`
+
+const ImgLivro = styled.img`
+    width: 150px;
+`
+
+function CardRecomenda({titulo, subtitulo, descricao, src}) {
     return (
-        <div>
-            <h3>Talvez você se interesse por...</h3>
-            <h4>Angular 11</h4>
-            <p>Construindo uma aplicação integrada</p>
-            <img />
-            <button>Saiba mais</button>
-        </div>
+        <Card>
+            <div>
+                <Titulo tamanhoFonte="16px" cor="#EB9B00" alinhamento="left" paddingTopBot="6px">{titulo}</Titulo>
+                <Subtitulo>{subtitulo}</Subtitulo>
+                <Descricao>{descricao}</Descricao>
+            </div>
+            <div>
+                <ImgLivro src={src}/>
+                <Botao>Saiba mais</Botao>
+            </div>
+        </Card>  
     )
 }
 
-export default CardRecomenda;
+export default CardRecomenda
