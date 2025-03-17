8da5d58defa5afb9281e08cee70646f0f6466801
Rafael Pequino <o.rafaelpequino@gmail.com>
2025-03-17 14:12:35 -0300

    Estiliza página de favoritos

--- src/routes/Favoritos.js
+++ src/routes/Favoritos.js
@@ -1,31 +1,79 @@
-import { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import styled from 'styled-components'
 import { getFavoritos } from '../services/favoritos';
 
+
 const AppContainer = styled.div`
-    width: 100%;
-    height: max-content;
+   max-width: 100vw;
+   height: 100vh;
+   background-image: linear-gradient(90deg,#002F52 35%,#326589 165%);
+`
+
+const ResultadoContainer = styled.div`
+   display: flex;
+   flex-wrap: wrap;
+   justify-content: center;
+`
+
+const Resultado = styled.div`
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   margin: 20px 0;
+   cursor: pointer;
+   text-align: center;
+   padding: 0 100px;
+   p {
+       width: 200px;
+       color: #FFF;
+   }
+   img {
+       width: 100px;
+   }
+   &:hover {
+       border: 1px solid white;
+   }
+`
+
+const Titulo = styled.h2`
+   color: #FFF;
+   font-size: 36px;
+   text-align: center;
+   width: 100%;
+   padding-top: 35px
 `
 
 function Favoritos() {
-  const [favoritos, setFavoritos] = useState([]);
-
-  async function fetchFavoritos() {
-    const favoritosDaApi =  await getFavoritos();
-    setFavoritos(favoritosDaApi)
-  }
-
-  useEffect( () => {
-    fetchFavoritos();
-  }, [])
-
-  return (
-    <AppContainer>
-      {favoritos.map( favorito => (
-        <p>{favorito.nome}</p>
-      ) )}
-    </AppContainer>
-  );
+ const [favoritos, setFavoritos] = useState([])
+  
+ async function fetchFavoritos() {
+  const favoritosDaApi =  await getFavoritos();
+  setFavoritos(favoritosDaApi)
 }
 
-export default Favoritos;
+useEffect( () => {
+  fetchFavoritos();
+}, [])
+
+ return (
+   <AppContainer>
+     <div>
+       <Titulo>Aqui estão seus livros favoritos:</Titulo>
+       <ResultadoContainer>
+         {
+           favoritos.map(favorito => (
+             <Resultado>
+               <p>{favorito.nome}</p>
+               <img src="/img/livro.png"/>
+             </Resultado>
+           ))
+         }
+       </ResultadoContainer>
+     </div>
+   </AppContainer>
+ );
+}
+
+
+export default Favoritos
